b96c5f82e7658d9579c2cbe771dae5ade1c222d1
mam1 <mam1@cornell.edu>
2018-04-05 00:08:46 -0400

    set pin assignments to match protoype board

--- press control.c
+++ press control.c
@@ -21,8 +21,11 @@ int main()
 
 /* initializations */
 	pause(200);								// wait 1 second
+	high(_STATUS_LED_BUS_MUX);				// free up vga io pins */
+
 	printf("press control version %i.%i starting\n\n", _MAJOR_VERSION_system, _MINOR_VERSION_system);
-	up();
+	up();									// retract ram
+	cycle();								// flash leds
 	printf("start switch monitor cog\n");
 	cog_run(watch_up_switch, 128);     		// start cog to monitor swiwtch input
 	printf("start dewll monitor cog\n");
@@ -71,6 +74,36 @@ void down()
 	return;
 }
 
+/* cycle lights */
+void cycle()
+{
+	int 	led[8] = {_LED_1, _LED_2, _LED_3, _LED_4, _LED_5, _LED_6, _LED_7, _LED_8};
+	int 	i, ii;
+
+	for (i=0,i<3,i++)
+	{
+		for (ii=0,ii<8,ii++) low(led[ii]);
+		pause(100);
+		for (ii=0,ii<8,ii++) high(led[ii]);
+			pause(100);
+		for (ii=0,ii<8,ii++) low(led[ii]);
+		pause(100);
+		for (ii=0,ii<8,ii++) high(led[ii]);
+			pause(100);
+		for (ii=0,ii<8,ii++) low(led[ii]);
+		pause(100);
+		for (ii=0,ii<8,ii++)
+		{
+			if(ii>0) low(led[ii-1]);
+			else low(led[8]);
+			high(led[ii]);
+			pause(100);
+		}
+		pause(100);
+	}
+	return;
+}
+
 /* watch switches */
 void watch_up_switch(void)
 {
@@ -88,7 +121,7 @@ void watch_up_switch(void)
 /* let user set a new dwell time */
 void set_dwell(void)
 {
-	int 			dip_switch[8] = {_DIP_0, _DIP_1, _DIP_2, _DIP_3, _DIP_4, _DIP_5, _DIP_6, _DIP_7};
+	int 			tswitch[8] = {_DIP_0, _DIP_1, _DIP_2, _DIP_3, _DIP_4, _DIP_5, _DIP_6, _DIP_7};
 	int 			value, i;
 
 	while (1)
@@ -96,7 +129,7 @@ void set_dwell(void)
 		value = 0;
 		for (i = 0; i < 8; i++)				// convert DIP switch setting to decimal number
 		{
-			if (input(dip_switch[i]))	
+			if (input(tswitch[i]))	
 				value += (int)pow(2, i);
 		}
 		dwell = value * 10;					// dwell in .1 seconds
--- press control.h
+++ press control.h
@@ -2,10 +2,10 @@
 #define PRESS_CONTROL_H_
 
 /* version */
-#define _MAJOR_VERSION_system	4
-#define _MINOR_VERSION_system	1
+#define _MAJOR_VERSION_system	5
+#define _MINOR_VERSION_system	0
 
-/* switch pins */
+/* foot switch pins */
 #define _DOWN_SWITCH		17
 #define _UP_SWITCH			18
 
@@ -13,7 +13,7 @@
 #define _EXTEND_SOLENOID	0
 #define _RETRACT_SOLENOID	1
 
-/* DIP switch pins */
+/* switch pins */
 #define _DIP_0				2
 #define _DIP_1				3
 #define _DIP_2				4
@@ -22,6 +22,22 @@
 #define _DIP_5				7
 #define _DIP_6				8
 #define _DIP_7				9
+
+/* LED pins */
+#define _LED_1				16
+#define _LED_2				17
+#define _LED_3				18
+#define _LED_4				19
+#define _LED_5				20
+#define _LED_6				21
+#define _LED_7				22
+#define _LED_8				23
+
+/* VGA disable pin */
+#define _STATUS_LED_BUS_MUX		15
+
+/* cycle leds */
+void cycle(void);
 
 /* ram control  */
 void up(void);
