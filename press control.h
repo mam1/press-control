c45683c55e9a8500c9bbb1894b1f4ae76336844c
mam1 <mam1@cornell.edu>
2018-05-01 14:22:22 -0400

    x

--- press control.h
+++ press control.h
@@ -6,12 +6,12 @@
 #define _MINOR_VERSION_system	0
 
 /* foot switch pins */
-#define _DOWN_SWITCH		30
-#define _UP_SWITCH			31
+#define _DOWN_SWITCH		0
+#define _UP_SWITCH			1
 
 /* solenoid control pins */
-#define _EXTEND_SOLENOID	0
-#define _RETRACT_SOLENOID	1
+#define _EXTEND_SOLENOID	18
+#define _RETRACT_SOLENOID	19s
 
 /* switch pins */
 #define _DIP_0				2
@@ -20,18 +20,18 @@
 #define _DIP_3				5
 #define _DIP_4				6
 #define _DIP_5				7
-#define _DIP_6				8
-#define _DIP_7				9
+#define _DIP_6				16
+#define _DIP_7				17
 
-/* LED pins */
-#define _LED_1				16
-#define _LED_2				17
-#define _LED_3				18
-#define _LED_4				19
-#define _LED_5				20
-#define _LED_6				21
-#define _LED_7				22
-#define _LED_8				23
+// /* LED pins */
+// #define _LED_1				16
+// #define _LED_2				17
+// #define _LED_3				18
+// #define _LED_4				19
+// #define _LED_5				20
+// #define _LED_6				21
+// #define _LED_7				22
+// #define _LED_8				23
 
 /* VGA disable pin */
 #define _STATUS_LED_BUS_MUX		15
